ade0e95862631da311e8f43dcdc14ffb4aa05437
odinokun <odinokun@gmail.com>
2019-09-11 10:50:53 +0300

    did menu

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -2,7 +2,7 @@ module.exports = function() {
 
   // begin menu
   $('.header-top__burger').on('click', function () {
-    $('#menu').toggleClass('active');
+    $('#menu, #header-top__burger').toggleClass('active');
     $('#main-wrap').toggleClass('no-scroll');
   });
   // end menu
